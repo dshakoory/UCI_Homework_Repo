0127da848d1b81f15b16cbed6833c476dbd050f2
Dorna Shakoory <51681206+dshakoory@users.noreply.github.com>
2019-10-30 15:53:07 -0700

    added part 2

--- Homework9/sqlalchemy-challenge/app.py
+++ Homework9/sqlalchemy-challenge/app.py
@@ -3,51 +3,56 @@ import sqlalchemy
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
-from flask import Flask , jsonify
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask , jsonify
 
-# Setting up the Database
+
+#setting up the dataset
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite",echo=False)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
+
+
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+
 session = scoped_session(sessionmaker(bind=engine))
 
 last_12months = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
 last_12months= dt.datetime.strptime(last_12months,"%Y-%m-%d")
 date_oneyearago = last_12months - dt.timedelta(days = 365)
 
-# Setting up the Flask
 
+#settin up the flask
 
 app = Flask(__name__)
-# Setting up the Flask
-############################################
 
 @app.route("/")
-def Hello():
+def welcome():
 
     return (
-        f"Your available Routes:<br/>"
-        f"last year's dates and records :<br/>"
+        f"Your Available Routes:<br/>"
+        f"Last year's temp observations:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"Stations list:<br/>"
+        f"Station list:<br/>"
         f"/api/v1.0/stations<br/>"
-        f"List of las year's Temperature Observations (tobs):<br/>"
+        f"Last's year's tem observation:<br/>"
         f"/api/v1.0/tobs<br/>"
-        f" Minimum temperature, Maximum temperature, and Average temperature :<br/>"
+        f"List of the minimum , average and the max temperature for start date:<br/>"
         f"/api/v1.0/<start><br/>"
-        f"Minimum temperature, Maximum temperature, and Average temperature for a given start and end dates:<br/>"
+        f"List of the minimum , average and the max temperature for end date:<br/>"
         f"/api/v1.0/<start>/<end><br/>"
     )
 
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-	
-	whole_12months = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date > date_oneyearago ).order_by(Measurement.date).all()
-	precipitation_data = dict(whole_12months)
 
+	whole_12months = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date > date_oneyearago ).order_by(Measurement.date).all()
+
+	precipitation_data = dict(whole_12months)
 	return jsonify({'Data':precipitation_data})
 
 @app.route("/api/v1.0/stations")
@@ -68,44 +73,30 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
 
-	twelve_month_tobs = session.query(
-    	Measurement.tobs,
-    	Measurement.date,
-    	Measurement.station    
+	twelve_month_tobs = session.query(Measurement.tobs,Measurement.date,Measurement.station).filter(Measurement.date > first_date).all()
 
-	).filter(
-    	Measurement.date > first_date
-	).all()
-
-	temp_list = list()
+	temperature_list = list()
 	for data in twelve_month_tobs:
-		temp_dict = dict()
-		temp_dict['Station'] = data.station
-		temp_dict['Date'] = data.date
-		temp_dict['Temp'] = data.tobs
-		temp_list.append(temp_dict)
+		temperature_dict = dict()
+		temperature_dict['Station'] = data.station
+		temperature_dict['Date'] = data.date
+		temperature_dict['Temp'] = data.tobs
+		temperature_list.append(temperature_dict)
 
-	return jsonify ({'Data':temp_list})
+	return jsonify ({'Data':temperature_list})
 
 @app.route("/api/v1.0/<start>")
 def start_temp(start=None):
 
-	start_temps = session.query(
-		func.min(Measurement.tobs), 
-		func.avg(Measurement.tobs),
-		func.max(Measurement.tobs)
-	).filter(
-		Measurement.date >= start
-	).all()
-
+	starting_temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
 
 	start_list = list()
-	for tmin, tavg, tmax in start_temps:
-		start_dict = {}
-		start_dict["Min Temp"] = tmin
-		start_dict["Max Temp"] = tavg
-		start_dict["Avg Temp"] = tmax
-		start_list.append(start_dict)
+	for lowest_temp, avg_temp, highest_temp in starting_temps:
+		starting_dict = {}
+		starting_dict["Min Temp"] = lowest_temp
+		starting_dict["Max Temp"] = avg_temp
+		starting_dict["Avg Temp"] = highest_temp
+		start_list.append(starting_dict)
 
 	return jsonify ({'Data':start_list})
 
@@ -113,21 +104,14 @@ def start_temp(start=None):
 
 @app.route("/api/v1.0/<start>/<end>")
 def calc_temps(start=None,end=None):
-    temps = session.query(
-        func.min(Measurement.tobs),
-        func.avg(Measurement.tobs),
-        func.max(Measurement.tobs)
-    ).filter(
-    	Measurement.date >= start,
-    	Measurement.date <= end
-    ).all()
+    temperatures = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start,Measurement.date <= end).all()
 
     temp_list = list()
-    for tmin, tavg, tmax in temps:
+    for lowest_temp, avg_temp, highest_temp in temperatures:
     	temp_dict = dict()
-    	temp_dict["Min Temp"] = tmin
-    	temp_dict["Avg Temo"] = tavg
-    	temp_dict["Max Temp"] = tmax
+    	temp_dict["Min Temp"] = lowest_temp
+    	temp_dict["Avg Temo"] = avg_temp
+    	temp_dict["Max Temp"] = highest_temp
     	temp_list.append(temp_dict)
 
     return jsonify ({'Data':temp_list})
